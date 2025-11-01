6b3ccf99fe6f8c7e5e210b4ce59158ef39cc9617
panso <121546982+Panos0210@users.noreply.github.com>
2025-11-01 18:15:31 +0200

    Update Schedule Shutdown.py

--- Source Code/Schedule Shutdown.py
+++ Source Code/Schedule Shutdown.py
@@ -4,7 +4,7 @@ shut = input('Is it shutdown or restart (s, r): ')
 time = int(input('Enter time to shutdown: '))
 type = input('Is it in hours, minutes or seconds (h, m, s): ')
 
-shut =shut.lower()
+shut = shut.lower()
 type = type.lower()
 
 if type == 'h':
